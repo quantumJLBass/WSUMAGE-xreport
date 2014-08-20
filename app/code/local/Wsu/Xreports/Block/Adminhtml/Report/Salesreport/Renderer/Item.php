12166d3271975f93a70c1914429eafe91f5b9cf0
Jeremy Bass <jeremybass@cableone.net>
2014-08-20 11:20:59 -0700

    visually display the options from the order

--- app/code/local/Wsu/Xreports/Block/Adminhtml/Report/Salesreport/Renderer/Item.php
+++ app/code/local/Wsu/Xreports/Block/Adminhtml/Report/Salesreport/Renderer/Item.php
@@ -8,31 +8,148 @@ class Wsu_Xreports_Block_Adminhtml_Report_Salesreport_Renderer_Item extends Mage
 		
 		$finalResult = array();
 		$model = Mage::getModel('sales/order')->load($id);
+		
+		$html="";
 		// Loop through all items in the cart
 		foreach ($model->getAllVisibleItems() as $item) {
 			$product = $item->getProduct();
 		  // Array to hold the item's options
 		  $result = array();
 		  // Load the configured product options
-		  $options = $product->getTypeInstance(true)->getOrderOptions($product);
+		  $options = $item->getProductOptions();
+		  //$finalResult = array_merge($finalResult, $options);
 		  // Check for options
 		  if (isset($options['info_buyRequest'])){
-			  $info = $options['info_buyRequest'];
-			/*if (isset($info['options'])){
-			  $result = array_merge($result, $info['options']);
-			}*/
-			if (isset($info['options']['additional_options'])){
-			  $result = array_merge($result, unserialize($info['options']['additional_options']) );
+			$info = $options['info_buyRequest'];
+			if (isset($info['options'])){
+			//	$result = array_merge($result, $info['options']);
 			}
-			/*if (!empty($info['attributes_info'])){
-			  $result = array_merge($info['attributes_info'], $result);
-			}*/
+			if (isset($info['options']['additional_options'])){
+				$result = array_merge($result, unserialize($info['options']['additional_options']) );
+			}
+			if (!empty($info['attributes_info'])){
+				$result = array_merge($info['attributes_info'], $result);
+			}
 		  }
 		  $finalResult = array_merge($finalResult, $result);
 		}
 		ob_start();
-		var_dump($finalResult);
-		$html = ob_get_clean(); 
+   ?><?php if (!empty($finalResult)):?>
+				
+				
+                	
+					<?php
+					
+					
+					
+					$options=array();
+					foreach ($finalResult as $_option){
+						$label = trim($this->escapeHtml($_option['label']));
+						$value = trim($_option['value']);
+						if ( $value!=="" && $label!=="" && strpos($label,'guest_')===false ){
+							$options[]=$_option;			
+						}
+					}
+					
+					if(!empty($options)):
+					?>
+				
+				
+				
+                	<h4><?=$this->__('Your options')?></h4>
+                    <dl>
+                        <?php foreach ($options as $_option) : 
+						$label = trim($this->escapeHtml($_option['label']));
+						$value = trim($_option['value']);
+						if ( $value!=="" && $label!=="" && strpos($label,'guest_')===false ): ?>
+                            <dt><?php
+								if(strpos($label,'firstName')!==false){
+									echo "First Name:";
+								}elseif(strpos($label,'lastName')!==false){
+									echo "Last Name:";
+								}elseif(strpos($label,'dietary')!==false){
+									echo "Dietary Choice:";
+								}elseif(strpos($label,'meal')!==false){
+									echo "Meal Choice:";
+								}elseif(strpos($label,'mobility')!==false){
+									echo "Mobility Requirements:";
+								}elseif(strpos($label,'requested_seating')!==false){
+									echo "Seating Request:";
+								}
+							 ?></dt>
+                            <dd><?=$_option['value']?></dd>
+                        <?php endif; ?>
+                        <?php endforeach; ?>
+                    </dl>
+					<?php endif;?>
+                <?php endif;?>
+		
+                <?php if (!empty($finalResult)):?>
+                	
+					<?php
+					$options=array();
+					foreach ($finalResult as $_option){
+						$label = trim($this->escapeHtml($_option['label']));
+						$value = trim($_option['value']);
+						if ( $label!=="" && strpos($label,'guest_')!==false 
+									&& strpos($label,'_{%d%}_')===false ){
+							$options[]=$_option;			
+						}
+					}
+					
+					if(!empty($options)):
+					?>
+					<h4><?=$this->__('Guest list')?></h4>
+					
+					
+					
+					
+                    	<?php $i=1; ?>
+                        <?php foreach ($options as $_option) : ?>
+							<?php //$_formatedOptionValue = $this->getFormatedOptionValue($_option) ?>
+                            <?php 
+                            $label = trim($this->escapeHtml($_option['label']));
+                            $value = trim($_option['value']);
+                            if ( $label!=="" && strpos($label,'guest_')!==false 
+									&& strpos($label,'_{%d%}_')===false ):
+									 ?>
+							<?php
+								
+								if(strpos($label,'guest_'.$i.'_')!==false){
+									if($i>0){
+										echo "</dl>";	
+									}
+									echo "Guest ".$i."<dl>";
+									$i++;
+								}
+							 ?>	
+								 <?php if ( $value!=="" ): ?>
+                                    <dt><?php
+                                        
+                                        if(strpos($label,'firstName')!==false){
+                                            echo "First Name:";
+                                        }elseif(strpos($label,'lastName')!==false){
+                                            echo "Last Name:";
+                                        }elseif(strpos($label,'dietary')!==false){
+                                            echo "Dietary Choice:";
+                                        }elseif(strpos($label,'meal')!==false){
+                                            echo "Meal Choice:";
+                                        }elseif(strpos($label,'mobility')!==false){
+                                            echo "Mobility Requirements:";
+                                        }elseif(strpos($label,'requested_seating')!==false){
+                                            echo "Seating Request:";
+                                        }
+                                     ?></dt>
+                                    <dd><?=$_option['value']?></dd>
+                                 <?php endif; ?>
+                        	<?php endif; ?>
+                        <?php endforeach; ?>
+                    </dl>
+					<?php endif;?>
+                <?php endif;?>
+		<?php
+		    
+		$html .= ob_get_clean(); 
 
         return $html;
     }
