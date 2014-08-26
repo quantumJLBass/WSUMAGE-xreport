0658d906550c437e532170f95701f7a6a0d79fe6
jeremyBass <jeremybass@cableone.net>
2014-08-26 15:59:54 -0700

    make sure that we are only adding options for the sku if filtered on sku

--- app/code/local/Wsu/Xreports/Helper/Data.php
+++ app/code/local/Wsu/Xreports/Helper/Data.php
@@ -167,6 +167,8 @@ class Wsu_Xreports_Helper_Data extends Mage_Core_Helper_Abstract {
 	// callback method
 	public function dynoColCallback($collection) {
 		$optionkeyarray=array();
+		$request = Mage::app()->getRequest();
+		$requestData = Mage::helper('adminhtml')->prepareFilterString($request->getParam('filter'));
 		try{
 			$newCollection = new Varien_Data_Collection();
 			foreach($collection as $_item){
@@ -176,25 +178,27 @@ class Wsu_Xreports_Helper_Data extends Mage_Core_Helper_Abstract {
 				// Loop through all items in the cart
 				foreach ($model->getAllVisibleItems() as $item) {
 					$product = $item->getProduct();
-					// Array to hold the item's options
-					$result = array();
-					// Load the configured product options
-					$options = $item->getProductOptions();
-					//$finalResult = array_merge($finalResult, $options);
-					// Check for options
-					if (isset($options['info_buyRequest'])){
-						$info = $options['info_buyRequest'];
-						if (isset($info['options'])){
-						//	$result = array_merge($result, $info['options']);
+					if(isset( $requestData['sku'] ) && strtolower($requestData['sku'])==strtolower($product->getSku()) || !isset( $requestData['sku'] )){
+						// Array to hold the item's options
+						$result = array();
+						// Load the configured product options
+						$options = $item->getProductOptions();
+						//$finalResult = array_merge($finalResult, $options);
+						// Check for options
+						if (isset($options['info_buyRequest'])){
+							$info = $options['info_buyRequest'];
+							if (isset($info['options'])){
+							//	$result = array_merge($result, $info['options']);
+							}
+							if (isset($info['options']['additional_options'])){
+								$result = array_merge($result, unserialize($info['options']['additional_options']) );
+							}
+							if (!empty($info['attributes_info'])){
+								$result = array_merge($info['attributes_info'], $result);
+							}
 						}
-						if (isset($info['options']['additional_options'])){
-							$result = array_merge($result, unserialize($info['options']['additional_options']) );
-						}
-						if (!empty($info['attributes_info'])){
-							$result = array_merge($info['attributes_info'], $result);
-						}
+						$finalResult = array_merge($finalResult, $result);
 					}
-					$finalResult = array_merge($finalResult, $result);
 				}
 				foreach ($finalResult as $_option){
 					$label = trim($this->escapeHtml($_option['label']));
